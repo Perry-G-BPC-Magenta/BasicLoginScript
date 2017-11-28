922bd5d64602be47d915c1580b356c7b2674e0e9
Perry-G-BPC-Magenta <32098875+Perry-G-BPC-Magenta@users.noreply.github.com>
2017-11-28 13:47:58 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+
+<form action="login.php" method="post">
+Username<input type="text" name="username">
+Password<input type="password" name="password">
+<input type="submit" value="Login">
+</form>
+
+?>
