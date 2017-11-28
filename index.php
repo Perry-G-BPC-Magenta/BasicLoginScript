5eca20322b3ab7d7966a94fc0886f48ef4531521
Perry-G-BPC-Magenta <32098875+Perry-G-BPC-Magenta@users.noreply.github.com>
2017-11-28 13:55:57 +0000

    Update and rename login.php to index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-<?php
-
+<html>
+<head>
+  <title> Login thing </title>
 <form action="login.php" method="post">
 Username<input type="text" name="username">
 Password<input type="password" name="password">
 <input type="submit" value="Login">
 </form>
-
-?>
+</body>
+</html
